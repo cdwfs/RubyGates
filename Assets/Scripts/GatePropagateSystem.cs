d523de0844c8ac00c6b0b52e7444234e99510539
Cort <1944792+cdwfs@users.noreply.github.com>
2020-04-13 18:17:41 -0700

    gate propagation works!

--- Assets/Scripts/GatePropagateSystem.cs
+++ Assets/Scripts/GatePropagateSystem.cs
@@ -1,15 +1,43 @@
-﻿using Unity.Entities;
+﻿using System.Collections.Generic;
+using Unity.Entities;
 
-[UpdateAfter(typeof(GateDagSortSystem))]
+[UpdateAfter(typeof(HandleInputSystem))]
 public class GatePropagateSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        Entities
-            .WithName("GatePropagateSystem")
-            .ForEach((ref NodeOutput output, in DynamicBuffer<NodeInput> inputs, in GateInfo gateInfo) =>
-            {
-                //output.Value += 1;
-            }).ScheduleParallel();
+        var nodeOutputs = GetComponentDataFromEntity<NodeOutput>(true);
+
+        var validNodeDepths = new List<DagDepth>();
+        EntityManager.GetAllUniqueSharedComponentData(validNodeDepths);
+        
+        foreach(var depth in validNodeDepths)
+        {
+            if (depth.Value == 0)
+                continue; // skip depth-zero nodes; they should all be buttons.
+            Entities
+                .WithName("GatePropagateSystem")
+                .WithNativeDisableContainerSafetyRestriction(nodeOutputs) // DAG sort ensures each pass writes to different outputs than it reads
+                .WithSharedComponentFilter(depth)
+                .ForEach((ref NodeOutput output, in DynamicBuffer<NodeInput> inputs, in GateInfo gateInfo) =>
+                {
+                    output.PrevValue = output.Value;
+                    switch (gateInfo.Type)
+                    {
+                        case GateType.And:
+                            output.Value = nodeOutputs[inputs[0].InputEntity].Value & nodeOutputs[inputs[1].InputEntity].Value;
+                            break;
+                        case GateType.Or:
+                            output.Value = nodeOutputs[inputs[0].InputEntity].Value | nodeOutputs[inputs[1].InputEntity].Value;
+                            break;
+                        case GateType.Xor:
+                            output.Value = nodeOutputs[inputs[0].InputEntity].Value ^ nodeOutputs[inputs[1].InputEntity].Value;
+                            break;
+                        case GateType.Not:
+                            output.Value = 1 - nodeOutputs[inputs[0].InputEntity].Value;
+                            break;
+                    }
+                }).ScheduleParallel();
+        }
     }
 }
