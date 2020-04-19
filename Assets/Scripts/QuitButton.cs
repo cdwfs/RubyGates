5614e7c8107c7592c6829f63301c0af65b990740
Cort <1944792+cdwfs@users.noreply.github.com>
2020-04-18 17:20:13 -0700

    Add UI canvas w/Quit button to every level

---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    public static void OnClick()
+    {
+        if (Application.isEditor)
+        {
+            Debug.Log("In standalone builds, that would quit the game.");
+#if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+#endif
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
