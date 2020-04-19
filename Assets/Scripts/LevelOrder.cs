8d09d7f3bb6a30523a369f830e9f45d9a90be725
Cort <1944792+cdwfs@users.noreply.github.com>
2020-04-19 09:14:12 -0700

    Store level order in a central ScriptableObject

---
+++ Assets/Scripts/LevelOrder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/LevelOrder", order = 1)]
+public class LevelOrder : ScriptableObject
+{
+    public string[] levels;
+}
--- Assets/Scripts/Victory.cs
+++ Assets/Scripts/Victory.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 [DisallowMultipleComponent]
 public class Victory : MonoBehaviour
 {
-    public string nextLevelName;
+    public LevelOrder levelOrder;
 
     private ParticleSystem _particles;
     // Start is called before the first frame update
@@ -23,6 +23,17 @@ public class Victory : MonoBehaviour
     IEnumerator VictoryCoroutine() {
         _particles.Play();
         yield return new WaitForSeconds(_particles.main.duration + 2.0f);
-        SceneManager.LoadScene(nextLevelName);
+        var currentSceneName = SceneManager.GetActiveScene().name;
+        int currentIndex = Array.FindIndex(levelOrder.levels, l => l == currentSceneName);
+        if (currentIndex == -1)
+            throw new ArgumentException($"Current Scene {currentSceneName} not found in LevelOrder");
+        int nextIndex = currentIndex + 1;
+        if (nextIndex >= levelOrder.levels.Length)
+        {
+            Debug.LogError($"next level {nextIndex} is out of range [0..{levelOrder.levels.Length - 1}]. Returning to level 0 instead.");
+            nextIndex = 0;
+        }
+
+        SceneManager.LoadScene(levelOrder.levels[nextIndex]);
     }
 }
