ccb40d5045f690f1af998fb1394f794500174e16
Cort <1944792+cdwfs@users.noreply.github.com>
2020-10-30 11:24:11 -0700

    Remove obsolete [RequiresEntityConversion] attribute

--- Assets/Scripts/Authoring/Wire.cs
+++ Assets/Scripts/Authoring/Wire.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 // This currently exists so that GateInputs can refer to a Wire prefab
 [DisallowMultipleComponent]
-[RequiresEntityConversion]
 public class Wire : MonoBehaviour
 {
 }
